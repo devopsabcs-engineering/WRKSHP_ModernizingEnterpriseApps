0ff831d0409edc2fc524ccfde0cc3c65c0cdf23d
Emmanuel.Knafo <Emmanuel.Knafo@cad4devops.com>
2024-09-30 06:48:36 -0400

    initial commit MVC core .NET 8

---
+++ Application/SampleWebApplicationCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SampleWebApplicationCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
